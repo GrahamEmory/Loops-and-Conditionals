4af39f721dd5fd7e01b493fccbd9ed547a26f0e7
GrahamEmory <gemory44@gmail.com>
2020-08-04 19:41:47 +0000

    Added for loops, while loops and if, else, and elif conditional statements.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 print("Loops and Conditionals exercise")
+
+# For loop
+# x is a loop variable
+for x in range(5):
+  print(x)
+
+for x in range(3, 6):
+  print(x)
+
+
+
+# While loop 
+count = 0 
+while (count < 3):
+  print("Hello friend!")
+  print
+  count += 1
+  
+# CONDITIONALS
+
+i = 15
+if (i > 15):
+  print("10 is less than 15.")
+  print("i'm in an if block")
+elif (i == 15): 
+  print("i is equal to 15")
+  print("i'm in an elif")
+else:
+  print("i is less than 15")
+  print("i'm in an else block")
